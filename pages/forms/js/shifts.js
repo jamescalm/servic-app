30a6dc54c598d82602a2311147e6e6b6fd3a8ed3
jamescalm <61954281+jamescalm@users.noreply.github.com>
2020-03-13 17:45:56 +0800

    Update shifts.js

--- pages/forms/js/shifts.js
+++ pages/forms/js/shifts.js
@@ -1,4 +1,5 @@
-shiftForm();
+
+
 //load form
 function shiftForm(){
   //load driver
@@ -37,8 +38,8 @@ function shiftForm(){
 
       data.forEach(setup => {
         shiftsTable = setup.data();
-
-        if (y == data.length-1){
+        console.log(data.length);
+        if (z == data.length-1){
           json3 += JSON.stringify(shiftsTable);
         }else{
           json3 += JSON.stringify(shiftsTable)+",";
@@ -47,8 +48,9 @@ function shiftForm(){
       });
       json3 += `]}`;
       obj3 = JSON.parse(json3);
-      var driverName = document.getElementById('driverSelect').value;
-
+      console.log(obj)
+      console.log(obj3)
+      var driverName;
       for( i = 0 ; i < y ; i++){
         if(z==0){
           if(obj.dataset[i].userTag == "Driver"){
@@ -58,7 +60,7 @@ function shiftForm(){
             html += tr;
           }
           document.getElementById("driverSelect").innerHTML = html;
-
+          driverName = document.getElementById('driverSelect').value;
           if(obj.dataset[i].name == driverName){
             document.getElementById("licenseID").value = obj.dataset[i].licenseNo;
             document.getElementById("userID").value = obj.dataset[i].id;
@@ -66,7 +68,9 @@ function shiftForm(){
           }
         }
         for(k = 0; k < z; k++){
-          var compareName = obj3.dataset[k].driverInfo.driverName == driverName;
+          driverName = document.getElementById('driverSelect').value;
+          var requestDate = document.getElementById('dateStart').value;
+          var compareName = obj3.dataset[k].driverInfo.driverName == obj.dataset[i].name;
 
           var dateRef = new Date(requestDate);
           var dateFrom = new Date(obj3.dataset[k].shiftDate.from);
@@ -74,9 +78,10 @@ function shiftForm(){
           var compDate = (dateRef <= dateTo)&&(dateRef >= dateFrom);
 
           var compare = compareName&&compDate;
-
+          console.log(compare);
           if(!compare){
             if(obj.dataset[i].userTag == "Driver"){
+              console.log(obj.dataset[i].name);
               const tr = `
               <option>${obj.dataset[i].name}</option>
               `;
@@ -84,16 +89,13 @@ function shiftForm(){
             }
             document.getElementById("driverSelect").innerHTML = html;
 
+            driverName = document.getElementById('driverSelect').value;
             if(obj.dataset[i].name == driverName){
               document.getElementById("licenseID").value = obj.dataset[i].licenseNo;
               document.getElementById("userID").value = obj.dataset[i].id;
-              console.log(document.getElementById("userID").value)
             }
           }
-
-
         }
-
       }
     }
 
@@ -101,6 +103,9 @@ function shiftForm(){
   }
 
 
+}
+
+function loadVehicle(){
   //load vehicle
   db.collection('vehicleTable').onSnapshot(snapshot =>{
     setupVehicle(snapshot.docs);
@@ -128,29 +133,98 @@ function shiftForm(){
     obj1 = JSON.parse(json1);
     console.log(obj1)
 
-    for( i = 0 ; i < x ; i++){
-      const tr = `
-      <option>${obj1.dataset[i].plateNo}</option>
-      `;
-      html += tr;
-      document.getElementById("plateID").innerHTML = html;
+    db.collection('shiftsTable').where('status','==','Pending').onSnapshot(snapshot =>{
+      setupShifts(snapshot.docs);
+    })
 
-      var plate = document.getElementById('plateID').value;
-      if(obj1.dataset[i].plateNo == plate){
-        document.getElementById("vehicleType").value = obj1.dataset[i].vehicleType;
-        document.getElementById("vehicleModel").value = obj1.dataset[i].model;
-        document.getElementById("paxNo").value = obj1.dataset[i].paxNo;
-        const img = `
-          <img id="Img" src = '${obj1.dataset[i].vehicleImg}' style = "max-width: 100%; max-height: 100%;">
-        `;
+    const setupShifts = (data) =>{
+      var z = 0;
+      var json2 =`{"dataset": [`;
+      var obj2;
 
-        document.getElementById("vehicleImg").innerHTML = img;
+      data.forEach(setup => {
+        shiftsTable = setup.data();
+        console.log(data.length);
+        if (z == data.length-1){
+          json2 += JSON.stringify(shiftsTable);
+        }else{
+          json2 += JSON.stringify(shiftsTable)+",";
+        }
+        z++;
+      });
+      json2 += `]}`;
+      obj2 = JSON.parse(json2);
+
+
+      for( i = 0 ; i < x ; i++){
+        if(z==0){
+          const tr = `
+          <option>${obj1.dataset[i].plateNo}</option>
+          `;
+          html += tr;
+          document.getElementById("plateID").innerHTML = html;
+
+          var plate = document.getElementById('plateID').value;
+          if(obj1.dataset[i].plateNo == plate){
+            document.getElementById("vehicleType").value = obj1.dataset[i].vehicleType;
+            document.getElementById("vehicleModel").value = obj1.dataset[i].model;
+            document.getElementById("paxNo").value = obj1.dataset[i].paxNo;
+            const img = `
+              <img id="Img" src = '${obj1.dataset[i].vehicleImg}' style = "max-width: 100%; max-height: 100%;">
+            `;
+
+            document.getElementById("vehicleImg").innerHTML = img;
+          }
+        }
+        for(k = 0; k < z; k++){
+          var comparePlate = (obj1.dataset[i].plateNo==obj2.dataset[k].carInfo.plateNo);
+          var requestDate = document.getElementById('dateStart').value;
+          var dateRef = new Date(requestDate);
+          var dateFrom = new Date(obj2.dataset[k].shiftDate.from);
+          var dateTo = new Date(obj2.dataset[k].shiftDate.to);
+          var compDate = (dateRef <= dateTo)&&(dateRef >= dateFrom);
+
+          var requestTime = document.getElementById('timeStart').value;
+          var requestTime1 = document.getElementById('timeEnd').value;
+          var timeRef = new Date('01/01/2000'+' '+requestTime);
+          var timeRef1 = new Date('01/01/2000'+' '+requestTime1);
+          var timeFrom = new Date('01/01/2000'+' '+obj2.dataset[k].shiftTime.from);
+          var timeTo = new Date('01/01/2000'+' '+obj2.dataset[k].shiftTime.to);
+          var compTime = (timeRef < timeTo)&&(timeRef >= timeFrom);
+          var compTime1 = (timeRef1 <= timeTo)&&(timeRef1 > timeFrom);
+          var compTime2 = (timeRef <= timeFrom)&&(timeRef1 >= timeTo);
+          var compareTime = compTime||compTime1||compTime2
+          var compDateTime = compDate && compareTime;
+
+          var compare1 = (comparePlate&&compDateTime)
+          console.log(compTime)
+          if(!compare1){
+            const tr = `
+            <option>${obj1.dataset[i].plateNo}</option>
+            `;
+            html += tr;
+            document.getElementById("plateID").innerHTML = html;
+
+            var plate = document.getElementById('plateID').value;
+            if(obj1.dataset[i].plateNo == plate){
+              document.getElementById("vehicleType").value = obj1.dataset[i].vehicleType;
+              document.getElementById("vehicleModel").value = obj1.dataset[i].model;
+              document.getElementById("paxNo").value = obj1.dataset[i].paxNo;
+              const img = `
+                <img id="Img" src = '${obj1.dataset[i].vehicleImg}' style = "max-width: 100%; max-height: 100%;">
+              `;
+
+              document.getElementById("vehicleImg").innerHTML = img;
+            }
+          }
+        }
+
       }
     }
 
   }
-
 }
+
 var setDate = document.getElementById('setDate');
 setDate.addEventListener('click', (e) =>{
   e.preventDefault();
@@ -161,14 +235,30 @@ setDate.addEventListener('click', (e) =>{
     document.getElementById('timeStart').disabled = false;
     document.getElementById('timeEnd').disabled = false;
     document.getElementById('dayoff').disabled = false;
-    document.getElementById('plateID').disabled = false;
+
+    shiftForm();
     chooseDayoff();
   }else{
     document.getElementById('driverSelect').disabled = true;
     document.getElementById('timeStart').disabled = true;
     document.getElementById('timeEnd').disabled = true;
     document.getElementById('dayoff').disabled = true;
+  }
+
+});
+
+var setDriver = document.getElementById('setDriver');
+setDriver.addEventListener('click', (e) =>{
+  e.preventDefault();
+  var comptimestart = document.getElementById('timeStart').value == '';
+  var comptimeend = document.getElementById('timeEnd').value == '';
+  if(!(comptimestart||comptimeend)){
+    document.getElementById('plateID').disabled = false;
+    document.getElementById('submitShift').disabled = false;
+    loadVehicle()
+  }else{
     document.getElementById('plateID').disabled = true;
+    document.getElementById('submitShift').disabled = true;
   }
 
 });
@@ -256,6 +346,7 @@ function showVehicleDetails(){
 
   }
 }
+
 function chooseDayoff(){
   var dateData1 = document.getElementById('dateStart').value;
   var month1 = new Date(dateData1).getMonth()+1;
@@ -290,9 +381,8 @@ function test(){
 //add data to shiftsTable
 var submit = document.getElementById('submitShift');
 
-submit.addEventListener('submit', (e) =>{
+submit.addEventListener('click', (e) =>{
   e.preventDefault();
-  console.log()
   var driverName = document.getElementById('driverSelect').value;
   var licenseNo = document.getElementById('licenseID').value;
   var uid = document.getElementById("userID").value;
