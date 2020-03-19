2aee6fa3f5a35606c4f370e14660adcc5df81f05
jamescalm <61954281+jamescalm@users.noreply.github.com>
2020-03-19 17:45:17 +0800

    Update carLogs.js

--- pages/forms/js/carLogs.js
+++ pages/forms/js/carLogs.js
@@ -87,19 +87,56 @@ function carLogForm(a){
       }
     }
   }
+  //get requests from database
+  var x = 0;
+  db.collection('vehicleLog').onSnapshot(snapshot =>{
+    setupRequest(snapshot.docs);
+  });
+  //count number of request
+  const setupRequest = (data) =>{
+    x = 0;
+    data.forEach(setup => {
+      request = setup.data();
 
+      x++;
+
+    });
+    var myVar = setInterval(myTimer, 1000);
+    function myTimer(){
+      var day = new Date().getDate();
+      var month = new Date().getMonth() + 1;
+      var year = new Date().getFullYear();
+      var hour = new Date().getHours();
+      var min = new Date().getMinutes();
+      var sec = new Date().getSeconds();
+      var months = ('0'+ month).slice(-2);
+      var days = ('0'+ day).slice(-2);
+      var hrs = ('0'+ hour).slice(-2);
+      var mins = ('0'+ min).slice(-2);
+      var secs = ('0'+ sec).slice(-2);
+
+      var carLogRef = months + '/' + days +'/' + year + ' ' + hrs + ':'+ mins + ':' + secs + ' '+'#'+ (x+1);
+      document.getElementById('carLogRef').value = carLogRef;
+
+      var carLogDate = months + '/' + days +'/' + year;
+      document.getElementById('logDate').value = carLogDate;
+
+      var carLogTime = hrs + ':'+ mins + ':' + secs;
+      document.getElementById('logTime').value = carLogTime;
+    }
+  }
 }
 
 function showVehicleDetails(){
   db.collection('vehicleTable').onSnapshot(snapshot =>{
     setupVehicle(snapshot.docs);
   })
-  var x = 0;
-  var json =`{"dataset": [`;
-  var obj;
+
   const vehicleList = document.querySelector('.vehicleTable');
   const setupVehicle = (data) =>{
-
+    var x = 0;
+    var json =`{"dataset": [`;
+    var obj;
     let html =' ';
 
     data.forEach(setup => {
@@ -134,4 +171,65 @@ function showVehicleDetails(){
     }
 
   }
+}
+var sMileage =document.getElementById('sMileage');
+var eMileage =document.getElementById('eMileage');
+var file;
+var file2;
+sMileage.addEventListener('change', function(e){
+  e.preventDefault();
+  //get file
+  file = e.target.files[0];
+  console.log(file)
+  document.getElementById('filename').innerHTML = file.name;
+});
+eMileage.addEventListener('change', function(e){
+  e.preventDefault();
+  //get file
+  file2 = e.target.files[0];
+  console.log(file2)
+  document.getElementById('filename2').innerHTML = file2.name;
+});
+function uploadImg(a,b,c){
+  // var file = sMileage.target.files[0];
+  var day = new Date().getDate();
+  var month = new Date().getMonth() + 1;
+  var yyyy = new Date().getFullYear();
+  var hour = new Date().getHours();
+  var min = new Date().getMinutes();
+  var sec = new Date().getSeconds();
+  var MM = ('0'+ month).slice(-2);
+  var dd = ('0'+ day).slice(-2);
+  var HH = ('0'+ hour).slice(-2);
+  var mm = ('0'+ min).slice(-2);
+  var ss = ('0'+ sec).slice(-2);
+  var x;
+  if(a.id =='sMileage'){
+    x = file;
+  }
+  else if(a.id =='eMileage'){
+    x = file2;
+  }
+  console.log(x)
+  var filename = MM + dd + yyyy + HH + mm + ss + x.name;
+  console.log(filename,a.id,b);
+  var storeRef = storage.ref('vehicle_log/' + filename);
+
+  var task = storeRef.put(x);
+
+  //progress bars
+  task.on('state_changed',
+    function progress(snapshot){
+      var percentage = (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+      b.value = percentage;
+    },
+    function error(err){
+      console.log(err);
+    },
+    function complete(){
+      task.snapshot.ref.getDownloadURL().then(function(dlURL){
+        document.getElementById(c.id).value = dlURL;
+      });
+    }
+  );
 }
