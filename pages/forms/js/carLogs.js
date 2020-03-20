63fbf6e38c7536d3866a63ff34b22d4f8d8f5922
jamescalm <61954281+jamescalm@users.noreply.github.com>
2020-03-20 17:37:52 +0800

    Update carLogs.js

--- pages/forms/js/carLogs.js
+++ pages/forms/js/carLogs.js
@@ -89,7 +89,7 @@ function carLogForm(a){
   }
   //get requests from database
   var x = 0;
-  db.collection('vehicleLog').onSnapshot(snapshot =>{
+  db.collection('carLogsTable').onSnapshot(snapshot =>{
     setupRequest(snapshot.docs);
   });
   //count number of request
@@ -174,8 +174,10 @@ function showVehicleDetails(){
 }
 var sMileage =document.getElementById('sMileage');
 var eMileage =document.getElementById('eMileage');
+var fuelReciept =document.getElementById('fuelReciept');
 var file;
 var file2;
+var file3;
 sMileage.addEventListener('change', function(e){
   e.preventDefault();
   //get file
@@ -189,6 +191,13 @@ eMileage.addEventListener('change', function(e){
   file2 = e.target.files[0];
   console.log(file2)
   document.getElementById('filename2').innerHTML = file2.name;
+});
+fuelReciept.addEventListener('change', function(e){
+  e.preventDefault();
+  //get file
+  file3 = e.target.files[0];
+  console.log(file3)
+  document.getElementById('filename3').innerHTML = file3.name;
 });
 function uploadImg(a,b,c){
   // var file = sMileage.target.files[0];
@@ -209,6 +218,8 @@ function uploadImg(a,b,c){
   }
   else if(a.id =='eMileage'){
     x = file2;
+  }else if(a.id =='fuelReciept'){
+    x = file3;
   }
   console.log(x)
   var filename = MM + dd + yyyy + HH + mm + ss + x.name;
@@ -233,3 +244,99 @@ function uploadImg(a,b,c){
     }
   );
 }
+
+var addVehicleLog = document.getElementById('addVehicleLog');
+
+addVehicleLog.addEventListener('click', (e)=>{
+  e.preventDefault();
+  var driverName = document.getElementById('driverName').value;
+  var licenseNo = document.getElementById('licenseID').value;
+  var uid = document.getElementById("userID").value;
+  console.log(uid)
+
+  var plateNo = document.getElementById('plateID').value;
+  var vehicleType = document.getElementById('vehicleType').value;
+  var vehicleModel = document.getElementById('vehicleModel').value;
+  var paxNo = document.getElementById('paxNo').value;
+  var paxInput = Number(paxNo);
+  var vehicleImg = document.getElementById('Img').src;
+
+  var carLogRef = document.getElementById('carLogRef').value;
+  var logDate = document.getElementById('logDate').value;
+  var logTime = document.getElementById('logTime').value;
+
+  var sMileageInput = document.getElementById('startMileage').value;
+  var startMileage = Number(sMileageInput);
+  var startMileageURL = document.getElementById('startMileageURL').value;
+  var eMileageInput = document.getElementById('endMileage').value;
+  var endMileage = Number(eMileageInput);
+  var endMileageURL = document.getElementById('endMileageURL').value;
+  var mileDiff = endMileage - startMileage;
+
+  var fuelUsedInput = document.getElementById('liters').value;
+  var fuelUsed = Number(fuelUsedInput);
+  var fuelTankedInput = document.getElementById('fuelTanked').value;
+  var fuelTanked = Number(fuelTankedInput);
+  var fuelType = document.getElementById('fuelType').value;
+  var fuelPriceInput = document.getElementById('fuelPrice').value;
+  var fuelPrice = Number(fuelPriceInput);
+  var fuelRecieptURL = document.getElementById('fuelRecieptURL').value;
+  var fuelProductInit = fuelTanked * fuelPrice;
+  var fuelProductStr = fuelProductInit.toFixed(2);
+  var fuelProduct = Number(fuelProductStr);
+  console.log(mileDiff)
+  db.collection('carLogsTable').add({
+    driverInfo: {
+      driverName: driverName,
+      licenseNo: licenseNo,
+      driverID: uid,
+      status: ''
+    },
+    carInfo:{
+      plateNo: plateNo,
+      vehicleType: vehicleType,
+      model: vehicleModel,
+      paxNo: paxInput,
+      vehicleImg: vehicleImg
+    },
+    logDate:logDate,
+    logTime:logTime,
+    mileage:{
+      startMileage: startMileage,
+      startMileageURL: startMileageURL,
+      endMileage: endMileage,
+      endMileageURL: endMileageURL,
+      mileDiff: mileDiff,
+    },
+    fuel:{
+      fuelUsed: fuelUsed,
+      fuelTanked: fuelTanked,
+      fuelType: fuelType,
+      fuelPrice: fuelPrice,
+      fuelProduct:fuelProduct,
+      fuelRecieptURL: fuelRecieptURL,
+    },
+    status: 'Pending'
+
+  }).then(function(docRef){
+      db.collection('carLogsTable').doc(docRef.id).update({
+        id: docRef.id
+      })
+      document.getElementById('startMileage').value = "";
+      document.getElementById('startMileageURL').value = "";
+      document.getElementById('endMileage').value = "";
+      document.getElementById('endMileageURL').value = "";
+      document.getElementById('liters').value = "";
+      document.getElementById('fuelTanked').value = "";
+      document.getElementById('fuelType').value = "";
+      document.getElementById('fuelPrice').value = "";
+      document.getElementById('fuelRecieptURL').value = "";
+
+      window.alert("Data added to Car Log List");
+  }).catch(function(error){
+  // An error happened.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  window.alert(errorMessage);
+  });
+})
