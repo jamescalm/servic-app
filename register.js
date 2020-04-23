f7db331456ff7d3fe78c0ed5596fd2fcd5b233c9
jamescalm <61954281+jamescalm@users.noreply.github.com>
2020-04-23 21:03:44 +0800

    Update register.js

--- register.js
+++ register.js
@@ -1,52 +1,4 @@
-// //upload img
-// var fileButton = document.getElementById('fileButton');
-// var uploader = document.getElementById('uploader');
-// var file;
-// fileButton.addEventListener('change', function(e){
-//   e.preventDefault();
-//   //get file
-//   file = e.target.files[0];
-//   console.log(file)
-// });
-// var imageUpload = document.getElementById('imageUpload');
-// imageUpload.addEventListener('click', uploadImg, false);
-// function uploadImg(e){
-//   e.preventDefault();
-//   var day = new Date().getDate();
-//   var month = new Date().getMonth() + 1;
-//   var yyyy = new Date().getFullYear();
-//   var hour = new Date().getHours();
-//   var min = new Date().getMinutes();
-//   var sec = new Date().getSeconds();
-//   var MM = ('0'+ month).slice(-2);
-//   var dd = ('0'+ day).slice(-2);
-//   var HH = ('0'+ hour).slice(-2);
-//   var mm = ('0'+ min).slice(-2);
-//   var ss = ('0'+ sec).slice(-2);
-//
-//   var filename = MM + dd + yyyy + HH + mm + ss + file.name;
-//   console.log(filename);
-//   var storeRef = storage.ref('profileImages/' + filename);
-//
-//   var task = storeRef.put(file);
-//
-//   //progress bars
-//   task.on('state_changed',
-//     function progress(snapshot){
-//       var percentage = (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
-//       uploader.value = percentage;
-//     },
-//     function error(err){
-//       console.log(err);
-//     },
-//     function complete(){
-//       task.snapshot.ref.getDownloadURL().then(function(dlURL){
-//         document.getElementById("profileURL").value = dlURL;
-//       });
-//     }
-//   );
-// }
-//register
+// register
 var register = document.getElementById("register");
 
 register.addEventListener('click', (e) =>{
@@ -103,20 +55,6 @@ register.addEventListener('click', (e) =>{
 
 
 })
-
-// document.getElementById('fileButton').addEventListener('change', handleFileSelect, false);
-//
-// function handleFileSelect(evt) {
-//     var files = evt.target.files;
-//     var f = files[0];
-//     var img = document.getElementById("profileImg");
-//     var reader = new FileReader();
-//     console.log(reader.result)
-//     reader.onloadend = function() {
-//        img.src = reader.result;
-//     }
-//     reader.readAsDataURL(f);
-// }
 
 function licenseHide() {
   var userTag = document.getElementById('user-tag').value;
